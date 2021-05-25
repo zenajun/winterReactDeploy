cb4525a7ecdca950823a8528209ab6df03c5b1f1
Zena Jun <zena.jun@gmail.com>
2021-05-25 19:49:19 -0400

    adding router to main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Cat from 'Cat';
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <header className="App-header">
         <h1>Deploying with Netlify</h1>
+        <Link to="/cat">See the cat!</Link>
       </header>
+      <Route to="/cat" component={Cat}></Route>
     </div>
+    </Router>
   );
 }
 
